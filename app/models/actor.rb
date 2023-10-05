80584040b9200834f2a7c2f642ee0909fd710c11
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-10-05 14:45:15 +0000

    Created Actor#characters instance method

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -11,4 +11,13 @@
 #  updated_at :datetime         not null
 #
 class Actor < ApplicationRecord
+  def characters
+    # Retrieve actor ID
+    the_id = self.id
+
+    # Retrieve movie relation where actor_id matches
+    matching_characters = Character.where({ :actor_id => the_id})
+    # Return ActiveRecord relation containing matching characters
+    return matching_characters
+  end
 end
