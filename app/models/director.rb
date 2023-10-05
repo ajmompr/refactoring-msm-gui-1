d19d4dcdb367d830388e8012cac70f46d4d366fe
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-10-05 14:33:48 +0000

    created filmography instance method

--- app/models/director.rb
+++ app/models/director.rb
@@ -11,4 +11,12 @@
 #  updated_at :datetime         not null
 #
 class Director < ApplicationRecord
+  def filmography
+    # Retrieve director ID
+    my_id = self.id
+    # Retrieve movie relation where director_id matches
+    matching_movies = Movie.where({ :director_id => my_id})
+    # return an ActiveRecord relation containing the matching movies
+    return matching_movies
+  end
 end
